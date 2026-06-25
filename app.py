3aa5d6acebef5cb8df71a561c222d598add7a5cc
anishupr47-git <anish.upr.47@gmail.com>
2026-06-25 17:56:54 +0545

    updated

--- app.py
+++ app.py
@@ -161,14 +161,18 @@ with tab3:
                 )
             # If the feature is numerical, we use a number input box
             else:
+                min_val = float(bound['min'])
+                max_val = float(bound['max'])
                 start_val = 0.0
-                if start_val < bound['min']:
-                    start_val = float(bound['min'])
+                if start_val < min_val:
+                    start_val = min_val
+                elif start_val > max_val:
+                    start_val = max_val
                     
                 input_data[feature] = st.number_input(
                     label=feature,
-                    min_value=bound['min'],
-                    max_value=bound['max'],
+                    min_value=min_val,
+                    max_value=max_val,
                     value=start_val,
                     key=f"input_{feature}"
                 )
